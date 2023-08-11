a6fc412bf7450acedcc04e0eafaf24d2fd2d22a4
nerogud <nero.ogude@yahoo.com>
2023-08-11 17:00:12 +0100

    login test without login credentials

---
+++ src/test/java/testCases/TC_005_LoginFunctionalityWithoutCredentials.java
@@ -0,0 +1,42 @@
+package testCases;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import pageObjects.HomePage;
+import pageObjects.LoginPage;
+import pageObjects.WelcomePage;
+import testBase.BaseClass;
+
+public class TC_005_LoginFunctionalityWithoutCredentials extends BaseClass{
+	
+	@Test
+	public void LoginFunctionalityWithoutCredentials() throws InterruptedException {
+		
+		logger.info("***Starting LoginFunctionalityWithoutCredentials Test***");
+		
+		WelcomePage wp = new WelcomePage(driver);
+		wp.clickLagosCheckBox();
+		wp.clickConfirmBtn();
+		
+		HomePage hp = new HomePage(driver);
+		Thread.sleep(3000);
+		hp.clickSignup();
+		Thread.sleep(2000);
+		
+		logger.info("*** login details intentionally kept blank***");
+		LoginPage lp = new LoginPage(driver);
+		//intentionally skip login details;
+		
+		lp.clickLoginBtn();
+		
+		logger.info("***Assert test***");
+		Assert.assertEquals(lp.getEmailRequiredText(), "Email is required"); 
+		Assert.assertEquals(lp.getPasswordRequiredText(), "Password is required"); 
+		
+	}
+	
+	
+	
+
+}

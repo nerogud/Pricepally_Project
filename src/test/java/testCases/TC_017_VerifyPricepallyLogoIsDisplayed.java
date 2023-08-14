d56ae0da38a797815fe4c674bbc4ab54a49e8a28
nerogud <nero.ogude@yahoo.com>
2023-08-14 08:11:00 +0100

    TC_017

---
+++ src/test/java/testCases/TC_017_VerifyPricepallyLogoIsDisplayed.java
@@ -0,0 +1,38 @@
+package testCases;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import pageObjects.HomePage;
+import pageObjects.LoginPage;
+import pageObjects.WelcomePage;
+import testBase.BaseClass;
+
+public class TC_017_VerifyPricepallyLogoIsDisplayed extends BaseClass{
+	
+	@Test
+	public void VerifyPricepallyLogoIsDisplayed() throws InterruptedException {
+		
+		logger.info("***Starting VerifyPricepallyLogoIsDisplayed Test***");
+		
+		WelcomePage wp = new WelcomePage(driver);
+		wp.clickLagosCheckBox();
+		wp.clickConfirmBtn();
+		
+		HomePage hp = new HomePage(driver);
+		Thread.sleep(3000);
+		hp.clickSignup();
+		Thread.sleep(2000);
+		
+		logger.info("*** login details intentionally kept blank***");
+		LoginPage lp = new LoginPage(driver);
+		
+		
+		
+		logger.info("***Assert test***");
+		Assert.assertTrue(lp.pricepallyLogoIsDisplayed());
+		
+	}
+	
+
+}
